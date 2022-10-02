0d34187acf23a87731e6c0186ec6ee7c3aac47fc
Priyanka Sharma <105537443+priyanka1551@users.noreply.github.com>
2022-10-03 00:05:20 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (12.52%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String convertToTitle(int n) {
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0){
+            int rem = (n-1)%26;
+            sb.append((char)('A' + rem));
+            n = (n-1)/26;
+        }
+        
+        sb.reverse();
+        return sb.toString();
+    }
+}
+    
+    
