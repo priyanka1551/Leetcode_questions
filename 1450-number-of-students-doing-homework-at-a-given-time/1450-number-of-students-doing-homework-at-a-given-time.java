5298ab4ab8a9e193940046431c6d3272ec6cceee
Priyanka Sharma <105537443+priyanka1551@users.noreply.github.com>
2022-10-10 13:30:46 +0530

    Time: 1 ms (48.81%), Space: 42.4 MB (21.27%) - LeetHub

---
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count=0;
+        int len=startTime.length;
+        for(int i=0;i<len;i++){
+            if(queryTime>=startTime[i] && queryTime<=endTime[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
