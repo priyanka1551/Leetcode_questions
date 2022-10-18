01fe5d084da4594a401fd85d7455c811a8dadbca
Priyanka Sharma <105537443+priyanka1551@users.noreply.github.com>
2022-10-19 01:11:35 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (25.39%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count=numBottles;
+        while(numBottles>=numExchange){
+            int x=numBottles/numExchange;
+            count+=x;
+            numBottles=x+(numBottles-numExchange*x);
+        }
+        return count;
+    }
+}
