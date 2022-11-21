bc4fa3e11592cd19d82b6b0b8a839830660dc805
Priyanka Sharma <105537443+priyanka1551@users.noreply.github.com>
2022-11-21 10:18:11 +0530

    Added solution - LeetHub

---
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            obj.segregateElements(a, n);
+            
+            for(int i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+            
+            System.out.println();
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    
+    public void segregateElements(int arr[], int n)
+    {
+        List<Integer> lst1=new ArrayList<>();
+        List<Integer> lst2=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<0){
+                lst1.add(arr[i]);
+            }
+            else{
+                lst2.add(arr[i]);
+            }
+        }
+        int m=0;
+        for(int j=0;j<lst2.size();j++){
+            arr[m++]=lst2.get(j);
+        }
+        for(int k=0;k<lst1.size();k++){
+            arr[m++]=lst1.get(k);
+        }
+    }
+}
